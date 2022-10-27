19579eef86ab13896b41f6526b3373e51c24d1fd
Juan Bautista Elias <bautielias01@gmail.com>
2022-10-27 18:53:26 -0300

    Imágenes y desarrollo de galeria

--- frontend/src/components/productos/index.jsx
+++ frontend/src/components/productos/index.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
+import React from "react";
+import IMG from "../../../images/1.jpg";
 
 export const ProductosLista = () => {
   return (
-    <div>
-      <h1>Productos</h1>
-    </div>
-  )
-}
+    //  ↓ esta anotacion (<>) se hace para que el componente NO retorne la clase padre
+    <>
+      <h1 className="title">PRODUCTOS</h1>
+      <div className="productos">
+        {/* producto 1 */}
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1> TITLE </h1>
+            <p>Categoria</p>
+            <p className="price">$8,00</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
+        </div>
 
+        {/* producto 2 */}
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1> TITLE </h1>
+            <p>Categoria</p>
+            <p className="price">$8,00</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
+        </div>
+        
+        {/* producto 3 */}
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1> TITLE </h1>
+            <p>Categoria</p>
+            <p className="price">$8,00</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
